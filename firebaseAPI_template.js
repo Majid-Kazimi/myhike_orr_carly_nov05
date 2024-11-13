255f32699be309b5c06608a7cb527c6024bb8c4f
Majid <mkazimi2@my.bcit.ca>
2024-11-12 18:34:29 -0800

    Fix the indentation.

--- firebaseAPI_template.js
+++ firebaseAPI_template.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyC8z_deQJHqhrNZSDBHmKH1w6LNDVpgWr4",
+    authDomain: "dtc05-comp1800-demo.firebaseapp.com",
+    projectId: "dtc05-comp1800-demo",
+    storageBucket: "dtc05-comp1800-demo.firebasestorage.app",
+    messagingSenderId: "120931225139",
+    appId: "1:120931225139:web:b37942dd96f7fc5bec1d46"
 };
 
 //--------------------------------------------
--- scripts/eachHike.js
+++ scripts/eachHike.js
@@ -70,12 +70,12 @@ function populateReviews() {
                 // Populate the star rating based on the rating value
                 
 	              // Initialize an empty string to store the star rating HTML
-								let starRating = "";
-								// This loop runs from i=0 to i<rating, where 'rating' is a variable holding the rating value.
+				let starRating = "";
+				// This loop runs from i=0 to i<rating, where 'rating' is a variable holding the rating value.
                 for (let i = 0; i < rating; i++) {
                     starRating += '<span class="material-icons">star</span>';
                 }
-								// After the first loop, this second loop runs from i=rating to i<5.
+				// After the first loop, this second loop runs from i=rating to i<5.
                 for (let i = rating; i < 5; i++) {
                     starRating += '<span class="material-icons">star_outline</span>';
                 }
